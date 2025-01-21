26a52d67e4276cb721aabe0f1d5b403e3770db02
antonbakhtin1607 <antonbakhtin1607@gmail.com>
2025-01-21 18:36:34 +0200

    made layout, deals-chart, upcoming events  and header with burger and profile.

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,17 @@
+import { ThemedLayoutV2, ThemedTitleV2 } from "@refinedev/antd";
+import Header from "./header";
+
+const Layout = ({ children }: React.PropsWithChildren) => {
+  return (
+    <div>
+      <ThemedLayoutV2
+        Header={Header}
+        Title={(titleProps) => <ThemedTitleV2 {...titleProps} text="Refine" />}
+      >
+        {children}
+      </ThemedLayoutV2>
+    </div>
+  );
+};
+
+export default Layout;
